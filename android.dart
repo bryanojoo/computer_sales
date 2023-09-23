5c68583ad13f366d974af3798a88b00d92b76bd7
bryanojoo <bryan.ojoo@gmail.com>
2023-09-23 18:08:24 +0300

    front end for android app

---
+++ android.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ComputerSalesShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Computer Sales Shop'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Computer Sales Shop!'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(ComputerSalesShopApp());
+}
+print ()
